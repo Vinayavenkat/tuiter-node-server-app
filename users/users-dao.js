41f137255d8f2ed1f27e69db9419777408895c01
Vinaya <vinaya@kayatech.com>
2023-06-10 21:22:18 -0500

    changes pt 3

--- users/users-dao.js
+++ users/users-dao.js
@@ -1,20 +1,22 @@
-let users = [];
+import people from './users.js'
+
+let users = people
 
 
 export const findAllUsers = () => users;
 
 
 export const findUserById = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- if (index !== -1) return users[index];
- return null;
+    const index = users.findIndex((u) => u._id === uid);
+    if (index !== -1) return users[index];
+    return null;
 };
 
 
 export const findUserByUsername = (username) => {
- const index = users.findIndex((u) => u.username === username);
- if (index !== -1) return users[index];
- return null;
+    const index = users.findIndex((u) => u.username === username);
+    if (index !== -1) return users[index];
+    return null;
 };
 
 
@@ -22,22 +24,25 @@ export const findUserByCredentials = (username, password) => {
     const index = users.findIndex((u) => u.username === username && u.password === password);
     if (index !== -1) return users[index];
     return null;
-  };
-  
+};
 
 
-export const createUser = (user) => users.push(user);
+export const createUser = (user) => {
+    const newUser = {...user, _id: (new Date()).getTime() + ''}
+    users.push(newUser);
+    return newUser;
+}
 
 
 export const updateUser = (uid, user) => {
- const index = users.findIndex((u) => u._id === uid);
- users[index] = { ...users[index], ...user };
- return {status: 'ok'}
+    const index = users.findIndex((u) => u._id === uid);
+    users[index] = { ...users[index], ...user };
+    return users[index] 
 };
 
 
 export const deleteUser = (uid) => {
- const index = users.findIndex((u) => u._id === uid);
- users.splice(index, 1);
- return {status: 'ok'}
+    const index = users.findIndex((u) => u._id === uid);
+    users.splice(index, 1);
+    return { status: 'ok' }
 };
