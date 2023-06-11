ec743e665f97938ee17e28255b60cad8825ef6bd
Vinaya <vinaya@kayatech.com>
2023-06-10 21:20:59 -0500

    changes pt 2

--- users/users-controller.js
+++ users/users-controller.js
@@ -1,41 +1,16 @@
 import people from './users.js'
+import * as usersDao from "./users-dao.js";
+
 let users = people
 const UserController = (app) => {
-   app.get('/api/users', findUsers);
-   app.get('/api/users/:uid', findUserById);
-   app.post('/api/users', createUser);
-   app.delete('/api/users/:uid', deleteUser);
-   app.put('/api/users/:uid', updateUser);
-
-
+    app.get('/api/users', findUsers)
+    app.get('/api/users/:uid', findUserById);
+    app.post('/api/users', createUser);
+    app.delete('/api/users/:uid', deleteUser);
+    app.put('/api/users/:uid', updateUser);
 }
 
-const deleteUser = (req, res) => {
-    const userId = req.params['uid'];
-    users = users.filter(usr => usr._id !== userId);
-    res.sendStatus(200);
-  }
-  
-  const findUsers = (req, res) => {
-    const users = usersDao.findAllUsers();
-    res.json(users)
-}
-
-const findUserById = (req, res) => {
-    const userId = req.params.uid;
-    const user = users
-      .find(u => u._id === userId);
-    res.json(user);
-  }
- 
-const createUser = (req, res) => {
-    const newUser = req.body;
-    newUser._id = (new Date()).getTime() + '';
-    users.push(newUser);
-    res.json(newUser);
-  }
-
-  const updateUser = (req, res) => {
+const updateUser = (req, res) => {
     const userId = req.params['uid'];
     const updates = req.body;
     const newUser = usersDao.updateUser(userId, updates)
@@ -43,8 +18,33 @@ const createUser = (req, res) => {
     console.log(users);
     res.sendStatus(200);
 }
-   
-    
-  
-  
+
+
+const deleteUser = (req, res) => {
+    const userId = req.params['uid'];
+    users = users.filter(usr => usr._id !== userId);
+    res.sendStatus(200);
+}
+
+
+const createUser = (req, res) => {
+    let newUser = req.body;
+    newUser ["_id"] = (new Date()).getTime() + '';
+    users.push(newUser);
+    res.json(newUser);
+    console.log(users);
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users
+        .find(u => u._id === userId);
+    res.json(user);
+}
+
+const findUsers = (req, res) => {
+    const users = usersDao.findAllUsers();
+    res.json(users)
+}
+
 export default UserController
