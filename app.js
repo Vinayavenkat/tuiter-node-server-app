a978daef76326793aa2b2a73acf92398f6f61b6d
Vinaya <vinaya@kayatech.com>
2023-06-16 15:03:57 -0500

    Assignment6 commit

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
 import express from 'express'
-import session from "express-session";
 import cors from 'cors'
+import session from "express-session";
 import HelloController from "./controllers/hello-controller.js"
-import UserController from "./users/users-controller.js"
+import UserController from './users/users-controller.js';
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import AuthController from "./users/auth-controller.js";
-import mongoose from 'mongoose';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
-
-
-const app = express()
-mongoose.connect(CONNECTION_STRING);
+import AuthController from './users/auth-controller.js';
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://rvvinaya:secure123@cluster0.leubwgo.mongodb.net/?retryWrites=true&w=majority" || "mongodb://127.0.0.1:27017/tuiter");
+const app = express();
 app.use(
-  session({
-    secret: "any string",
-    resave: false,
-    saveUninitialized: true,
-  })
- );
-
- app.use(
-  cors({
-    credentials: true,
-    origin: (origin, callback) => {
-      // Allow all origins
-      callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allow all CRUD operations
-    })
-  );
+    session({
+      secret: "any string",
+      resave: false,
+      saveUninitialized: true,
+    })
+   );
+app.use(
+    cors({
+      credentials: true,
+      origin: (origin, callback) => {
+        // Allow all origins
+        callback(null, true);
+      },
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], // Allow all CRUD operations
+    })
+  );
+  
+  
 
 app.use(express.json());
-
-const port = process.env.PORT || 4000;
+app.options('*', cors());
+const port = process.env.PORT || 4000;   
 app.use(express.json());
-
 TuitsController(app);
 HelloController(app)
 UserController(app)
 AuthController(app);
+app.listen(process.env.PORT|| 4000);
 
-app.listen(process.env.PORT || 4000)
-
-   
