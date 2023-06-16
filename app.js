282646feef213fcf905d9783368549415cc15318
Vinaya <vinaya@kayatech.com>
2023-06-16 17:15:03 -0500

    git changes

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
-
+import mongoose from 'mongoose';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
 
 
 const app = express()
-
+mongoose.connect(CONNECTION_STRING);
 app.use(
   session({
     secret: "any string",
--- users/users-controller.js
+++ users/users-controller.js
@@ -1,50 +1,48 @@
-import people from './users.js'
+import people from './users.js';
 import * as usersDao from "./users-dao.js";
 
-let users = people
+let users = people;
+
 const UserController = (app) => {
-    app.get('/api/users', findUsers)
-    app.get('/api/users/:uid', findUserById);
-    app.post('/api/users', createUser);
-    app.delete('/api/users/:uid', deleteUser);
-    app.put('/api/users/:uid', updateUser);
-}
+  app.get('/api/users', findUsers);
+  app.get('/api/users/:uid', findUserById);
+  app.post('/api/users', createUser);
+  app.delete('/api/users/:uid', deleteUser);
+  app.put('/api/users/:uid', updateUser);
+};
 
 const updateUser = (req, res) => {
-    const userId = req.params['uid'];
-    const updates = req.body;
-    const newUser = usersDao.updateUser(userId, updates)
-    req.session['currentUser'] = newUser;
-    console.log(users);
-    res.sendStatus(200);
-}
-
+  const userId = req.params.uid;
+  const updates = req.body;
+  const updatedUser = usersDao.updateUser(userId, updates);
+  req.session.currentUser = updatedUser;
+  console.log(users);
+  res.sendStatus(200);
+};
 
 const deleteUser = (req, res) => {
-    const userId = req.params['uid'];
-    users = users.filter(usr => usr._id !== userId);
-    res.sendStatus(200);
-}
-
+  const userId = req.params.uid;
+  users = users.filter((user) => user._id !== userId);
+  res.sendStatus(200);
+};
 
 const createUser = (req, res) => {
-    let newUser = req.body;
-    newUser ["_id"] = (new Date()).getTime() + '';
-    users.push(newUser);
-    res.json(newUser);
-    console.log(users);
-}
+  const newUser = req.body;
+  newUser._id = new Date().getTime().toString();
+  users.push(newUser);
+  console.log(users);
+  res.json(newUser);
+};
 
 const findUserById = (req, res) => {
-    const userId = req.params.uid;
-    const user = users
-        .find(u => u._id === userId);
-    res.json(user);
-}
+  const userId = req.params.uid;
+  const user = users.find((user) => user._id === userId);
+  res.json(user);
+};
 
 const findUsers = (req, res) => {
-    const users = usersDao.findAllUsers();
-    res.json(users)
-}
+  const allUsers = usersDao.findAllUsers();
+  res.json(allUsers);
+};
 
-export default UserController
+export default UserController;
